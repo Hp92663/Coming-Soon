80fbd5f4d10a88a0f085634a821f84538e977819
Hp92663 <hirenkhut92663@gmail.com>
2023-09-01 23:57:58 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+// .input-box input,
+// .input-box button {
+//     height: 100%;
+//     outline: none;
+//     border: none;
+//     border: 1px solid #fff;
+//     border-radius: 4px;
+//     background-color: rgba(255, 255, 255, 0.2);
+// }
+
+// .input-box input {
+//     width: 100%;
+//     padding: 0 15px;
+// }
+
+// .input::placeholder {
+//     color: #fff;
+// }
+
+// .input-box button {
+//     cursor: pointer;
+//     white-space: nowrap;
+//     padding: 0 20px;
+// }
