ec88ef30c7e2ee6a24fa6e3f9778033c104de46b
Hp92663 <hirenkhut92663@gmail.com>
2023-09-03 21:34:50 +0530

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const endDate = "04 September 2023 08:20:00 PM"
+const endDate = "04 September 2023 12:00:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
