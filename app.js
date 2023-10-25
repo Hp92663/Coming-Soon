c3833c3ea24aa9a55ec3889564b48278fba574bd
Hiren Khut <88971001+Hp92663@users.noreply.github.com>
2023-10-25 14:00:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const endDate = "25 Octomber 2023 12:00:00 PM"
+const endDate = "27 Octomber 2023 12:00:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
